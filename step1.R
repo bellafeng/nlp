a0f3ef5ec04f99b30db2827a21decc805781cf19
Bella Feng <bellafeng@gmail.com>
2018-03-13 12:25:15 -0700

    Getting data in and visualize

---
+++ step1.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyverse)
+library(purrr)
+library(readr)
+
+library(ggplot2)
+training_folder <- "C:/Users/gfeng/Documents/20news-bydate-train/"
+# Define a function to read all files from a folder into a data frame
+read_folder <- function(infolder) {
+  data_frame(file = dir(infolder, full.names = TRUE)) %>%
+    mutate(text = map(file, read_lines)) %>%
+    transmute(id = basename(file), text) %>%
+    unnest(text)
+}
+
+# Use unnest() and map() to apply read_folder to each subfolder
+raw_text <- data_frame(folder = dir(training_folder, full.names = TRUE)) %>%
+  unnest(map(folder, read_folder)) %>%
+  transmute(newsgroup = basename(folder), id, text)
+
+
+raw_text %>%
+  group_by(newsgroup) %>%
+  summarize(messages = n_distinct(id)) %>%
+  ggplot(aes(newsgroup, messages)) +
+  geom_col() +
+  coord_flip()
